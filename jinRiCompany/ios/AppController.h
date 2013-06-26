fe39444bbac3f1d27926cabb8f240cfeab6c34a2
liuyuyefz <liuyuyefz@gmail.com>
2013-06-26 21:57:33 +0800

    Initial Commit

---
+++ jinRiCompany/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  jinRiCompanyAppController.h
+//  jinRiCompany
+//
+//  Created by lyy on 13-6-26.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ jinRiCompany/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  jinRiCompanyAppController.h
+//  jinRiCompany
+//
+//  Created by lyy on 13-6-26.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
